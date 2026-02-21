311a0d0b5ccebfdc7b9bf1382e2d701398fe2614
Shayaan Zari <shayaanzari@gmail.com>
2026-02-21 11:07:41 -0600

    Optimize for sparse matrices using scipy.sparse

--- src/eigen.py
+++ src/eigen.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.linalg import eigh
+from scipy.sparse import lil_matrix
+from scipy.sparse.linalg import eigsh
 import argparse
 import sys
 
@@ -21,8 +23,9 @@ def build_2d_hamiltonian(N=20, potential='well'):
 	
 	dx = 1. / float(N) # grid spacing, can be arbitrary
 	inv_dx2 = float(N * N) # 1/dx^2
-	H = np.zeros((N*N, N*N), dtype=np.float64)
-	
+	#H = np.zeros((N*N, N*N), dtype=np.float64)
+	H = lil_matrix((N*N, N*N), dtype=np.float64)
+
 	# Helper function to map (i,j) -> linear index
 	def idx(i, j):
 		return i * N + j
@@ -58,7 +61,7 @@ def build_2d_hamiltonian(N=20, potential='well'):
 			if j < N-1: # right
 				H[row, idx(i, j+1)] = inv_dx2
 				
-	return H
+	return H.tocsr() # Compressed Sparse Row format
 	
 def solve_eigen(N=20, potential='well', n_eigs=None):
 	"""
@@ -83,15 +86,17 @@ def solve_eigen(N=20, potential='well', n_eigs=None):
 
 	H = build_2d_hamiltonian(N, potential)
 	# Solve entire spectrum (careful for large N)
-	vals, vecs = eigh(H)
-	# Sort
-	idx_sorted = np.argsort(vals)
-	vals_sorted = vals[idx_sorted]
-	vecs_sorted = vecs[:, idx_sorted]
-	if n_eigs is None:
-		return vals_sorted, vecs_sorted
-	else:
-		return vals_sorted[:n_eigs], vecs_sorted[:, :n_eigs]
+	#vals, vecs = eigh(H)
+	vals, vecs = eigsh(H, k=n_eigs, which='SM')
+	return vals, vecs
+    # Sort
+	#idx_sorted = np.argsort(vals)
+	#vals_sorted = vals[idx_sorted]
+	#vecs_sorted = vecs[:, idx_sorted]
+	#if n_eigs is None:
+    #   return vals_sorted, vecs_sorted
+	#else:
+    #   return vals_sorted[:n_eigs], vecs_sorted[:, :n_eigs]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -100,38 +105,41 @@ if __name__ == '__main__':
 
     # Adding arguments
     parser.add_argument(
-        "-N", type=int, default=20,
-        help="number of grid cells -- default: 20"
+        "--N", type=int, default=10,
+        help="number of grid cells -- default: 10"
     )
     parser.add_argument(
-        "-potential", type=str, default="well", choices=["well", "harmonic"],
+        "--potential", type=str, default="well", choices=["well", "harmonic"],
         help="type of potential to use -- choices: well, harmonic -- default: well"
     )
     parser.add_argument(
-        "-n_eigs", type=int, default=5,
+        "--n-eigs", type=int, default=5,
         help="number of eigenvalues to return -- default: 5"
+    )
+    parser.add_argument(
+        "--out", type=str, default=None,
+        help="location to save results -- default: ./eigs_N{N}.txt"
     )
 
     args = parser.parse_args()
+	
+    if args.out is None:
+        args.out = f"eigs_N{args.N}.txt"
 
-    # --- Sanity Checks ---
     if args.N <= 0:
         print(f"error: N must be a positive integer. input: {args.N}")
         sys.exit(1)
     
     if args.n_eigs <= 0:
-        print("error: n_eigs must be a positive integer.")
+        print("error: n-eigs must be a positive integer.")
         sys.exit(1)
 
     if args.n_eigs > args.N**2:
         print(f"error: requested {args.n_eigs} eigenvalues, but max eigenvalues is N^2, {args.N**2}")
         sys.exit(1)
 
-    # --- Execution ---
-    print(f"solving 2D hamiltonian with N={args.N}, and potential={args.potential}...")
-    
+    print(f"solving 2D hamiltonian with N={args.N}, and potential={args.potential}")
     vals, vecs = solve_eigen(N=args.N, potential=args.potential, n_eigs=args.n_eigs)
-    
     print(f"\nLowest {len(vals)} eigenvalues:", vals)
-
-    np.savetxt(f'eigs_N{args.N}.txt', vals)
+    np.savetxt(f'{args.out}', vals)
+    print(f"Saved to output file: {args.out}")
